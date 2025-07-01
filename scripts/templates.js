9146512839be7d267686553e9e39a354854371a2
SaschaKessler90 <sascha.k1809@gmail.com>
2025-07-01 18:07:06 +0200

    Working on the comments section

--- scripts/templates.js
+++ scripts/templates.js
@@ -1,6 +1,27 @@
+
+
 function getBookTemplate(index){
     let book = ourBooks[index];
     
+    ;
+    
+    let booksComments = [];
+    for (let indexC = 0; indexC < book.comments.length; indexC++) {
+        
+        booksComments.push(book.comments[indexC].comment);
+          console.table(booksComments)
+    }
+
+      let commentsHtml;
+  if (booksComments.length === 0) {
+    commentsHtml = "<p>No comments yet.</p>";
+  } else {
+    commentsHtml = "<ul>";
+    for (let i = 0; i < booksComments.length; i++) {
+      commentsHtml += "<li>" + booksComments[i] + "</li>";
+    }
+    commentsHtml += "</ul>";
+  }
 
     return `<div class="books_section">
     <h2>${book.name}</h2>
@@ -8,7 +29,8 @@ function getBookTemplate(index){
     <p>Autor : ${book.author}</p>
     <p>Preis : ${book.price.toFixed(2)} EUR</p>
     <p>Erschienen : ${book.publishedYear}</p>
-    
+    <p>Kommentare : ${booksComments}</p> 
+    ${commentsHtml}
     </div>`
    
 }
