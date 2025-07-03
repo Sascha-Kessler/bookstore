c569cb674c445defb248ba59bb5b731dc9187320
SaschaKessler90 <sascha.k1809@gmail.com>
2025-07-03 19:26:10 +0200

    Finished Comments Section

--- scripts/templates.js
+++ scripts/templates.js
@@ -2,26 +2,27 @@
 
 function getBookTemplate(index){
     let book = ourBooks[index];
-    
-    ;
-    
+    let booksCommentsUserName = [];
     let booksComments = [];
-    for (let indexC = 0; indexC < book.comments.length; indexC++) {
-        
+        for (let indexC = 0; indexC < book.comments.length; indexC++) {
+        booksCommentsUserName.push(book.comments[indexC].name);
         booksComments.push(book.comments[indexC].comment);
-          console.table(booksComments)
-    }
-
-      let commentsHtml;
-  if (booksComments.length === 0) {
-    commentsHtml = "<p>No comments yet.</p>";
-  } else {
-    commentsHtml = "<ul>";
-    for (let i = 0; i < booksComments.length; i++) {
-      commentsHtml += "<li>" + booksComments[i] + "</li>";
-    }
-    commentsHtml += "</ul>";
-  }
+        }
+    
+    let commentsHtml;
+        if (booksComments.length === 0) {
+        commentsHtml = "<p>No comments yet.</p>";
+        } else {
+        commentsHtml = '<ul>';
+        for (let i = 0; i < booksComments.length; i++) {
+        commentsHtml += `
+            <li>
+                <span class="comment_user">${booksCommentsUserName[i]}</span>:
+                <span class="comment_text">${booksComments[i]}</span>
+            </li>`;
+        }
+        commentsHtml += '</ul>';
+        }
 
     return `<div class="books_section">
     <h2>${book.name}</h2>
@@ -29,7 +30,6 @@ function getBookTemplate(index){
     <p>Autor : ${book.author}</p>
     <p>Preis : ${book.price.toFixed(2)} EUR</p>
     <p>Erschienen : ${book.publishedYear}</p>
-    <p>Kommentare : ${booksComments}</p> 
     ${commentsHtml}
     </div>`
    
